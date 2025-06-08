4575789c13251b0e86508c6b36febaf52df5ea28
Fedexhlz <fedex.hlz@gmail.com>
2025-06-07 21:58:12 -0300

    Se añade estilo completo a Home y MainHome

--- src/components/MainHome.jsx
+++ src/components/MainHome.jsx
@@ -1,9 +1,51 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 const MainHome = () => {
   return (
-    <div>MainHome</div>
-  )
-}
+    <div className='flex flex-1 flex-col justify-center items-center text-white bg-gray-800 text-center px-4 py-20'>
+      {/* <h2 className='text-4xl sm:text-5xl font-bold mb-4'>
+        ¡Hola Mundo! Soy <span className='text-sky-600'>Federico</span>
+      </h2>
+      <p className='text-lg sm:text-xl text-gray-300 max-w-xl'>
+        <span className='text-sky-600'>Desarrollador Full Stack</span> en
+        formación.
+      </p>
+      <p className='text-lg sm:text-xl text-gray-300 max-w-xl mb-6'>Transformando ideas en código y desafíos en oportunidades.</p> */}
+      <h2 className='text-4xl sm:text-5xl font-bold mb-4'>
+        <span className='text-purple-600'>console</span>.
+        <span className='text-lime-500'>log</span>(
+        <span className='text-sky-500'>"Hola, soy Federico"</span>);
+      </h2>
+      <p className='text-lg sm:text-xl text-gray-300 max-w-xl'>
+        Estudiante de <span className='italic'>Programación</span> de la{' '}
+        <span className='font-bold'>UTN - FRT</span>.
+      </p>
+      <p className='text-lg sm:text-xl text-gray-300 max-w-xl mb-6'>
+        Transformando ideas en código y desafíos en oportunidades.
+      </p>
+      <div className='flex flex-col sm:flex-row gap-4'>
+        <Link
+          to='/aboutme'
+          className='bg-gray-700 hover:bg-gray-600 text-white px-6 py-2 rounded transition'
+        >
+          Sobre mí
+        </Link>
+        <Link
+          to='/projects'
+          className='bg-gray-700 hover:bg-gray-600 text-white px-6 py-2 rounded transition'
+        >
+          Mis Proyectos
+        </Link>
+        <Link
+          to='/contact'
+          className='bg-gray-700 hover:bg-gray-600 text-white px-6 py-2 rounded transition'
+        >
+          Contactame
+        </Link>
+      </div>
+    </div>
+  );
+};
 
-export default MainHome
+export default MainHome;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,7 +5,7 @@ import Footer from '../components/Footer';
 
 const Home = () => {
   return (
-    <div>
+    <div className='flex flex-col min-h-screen'>
       <Header />
       <MainHome />
       <Footer />
